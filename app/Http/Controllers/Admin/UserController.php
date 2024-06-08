821b9235624913847ad6d3353a34ddcdfe2f19c9
tham@3f.team <E4as73NGDt2JUP7kc4ye>
2024-06-08 14:11:43 +0700

    Video #47

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -3,14 +3,17 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Log;
 
-class DashboardController extends Controller
+class UserController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard');
+        $users = User::latest()->paginate(5);
+
+        return view('admin.users.index', compact('users'));
     }
 }
